e58beb09fdc9b4a075e42ad11a0a021247747b5f
/dev/null <a.d.kamenev@yandex.ru>
2023-01-29 16:27:46 +0300

    task 2-hard complited

--- seminar1/homeTask2hard.py
+++ seminar1/homeTask2hard.py
@@ -1,2 +1,24 @@
 # Найдите сумму цифр любого вещественного или целого числа. 
 # Можно использовать decimal. Через строку решать нельзя. 
+
+try:
+    number = float(input("Введите число \n"))
+except:
+    print ("Это точно число?")
+
+if (number < 0):
+    number *= -1
+
+integer_number = number
+while (integer_number % 10 != 0):
+    integer_number *= 10
+print(integer_number)
+
+integer_number = int(integer_number)
+
+digit_sum = 0
+while (integer_number > 0):
+    digit_sum += integer_number%10
+    integer_number //= 10
+    
+print(f"Сумма всех цифр числа {number} равна {digit_sum}")
