92a11e6244679efc6190b74c62f1ed45a27605aa
/dev/null <a.d.kamenev@yandex.ru>
2023-02-01 20:15:35 +0300

    started 1st(10) task

--- seminar2/homeTask10.py
+++ seminar2/homeTask10.py
@@ -4,3 +4,13 @@
 # повернуты вверх одной и той же стороной. Выведите минимальное
 # количество монет, которые нужно перевернуть.
 
+from random import randint as rnd
+
+table = []
+def distribution (table):
+    table_limit = int(input("Введите ограничение по максимальному числу монет"))
+    table_size = rnd(1, table_limit)
+    for i in range (table_size):
+        table.append(rnd(0,1))
+    return table
+
