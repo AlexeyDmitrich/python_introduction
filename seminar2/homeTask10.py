23275ce53c492d3d30a2bf210485549685080bae
/dev/null <a.d.kamenev@yandex.ru>
2023-02-02 06:09:06 +0300

    1st task in progress

--- seminar2/homeTask10.py
+++ seminar2/homeTask10.py
@@ -6,11 +6,18 @@
 
 from random import randint as rnd
 
-table = []
-def distribution (table):
+
+def distribution ():
+    table = []
     table_limit = int(input("Введите ограничение по максимальному числу монет"))
     table_size = rnd(1, table_limit)
     for i in range (table_size):
         table.append(rnd(0,1))
     return table
 
+def heads_or_tails (table):
+    for coin in table:
+        
+
+table = distribution()
+print(table)
