568e749c4070b33291b942c96161bc4b3d6cb6a5
/dev/null <106831060+AlexeyDmitrich@users.noreply.github.com>
2023-02-02 15:16:58 +0300

    Update homeTask3optional.py
    optional task #3 (with predicates) started

--- seminar2/homeTask3optional.py
+++ seminar2/homeTask3optional.py
@@ -3,3 +3,43 @@
 # сами значения предикатов случайные, проверяем это утверждение 100 раз, 
 # с помощью модуля time выводим на экран , сколько времени отработала программа. 
 # В конце вывести результат проверки истинности этого утверждения.
+
+from random import randint as rnd
+
+predicates_right = []
+predicates_left = []
+length = rnd(5,25)
+for predicat in range(length):
+    new = rnd(0,1)
+    if (new == 1):
+        predicates_left.append (True)
+    else:
+        predicates_left.append (False)
+    
+for i in range(length): 
+    predicates_right.append (predicates_left[i])
+
+print(predicates_left)
+print(predicates_right)
+
+conunction = False # True
+disunction = False # True
+
+for predicat in predicates_left:
+    if (not(predicat == True)):   # ни один предикат не true
+        disunction = True
+        continue
+    else:
+        disunction = False   
+for predicat in predicates_right: 
+    if ((not predicat) == True):   # каждый непредикат обязательно true
+        conunction = True
+        continue        
+    else:
+        conunction = False   # если хотя бы один - тру, возвращаем фолс
+
+truthfulness = False
+if conunction == disunction:
+    truthfulness = True
+
+print(truthfulness)
