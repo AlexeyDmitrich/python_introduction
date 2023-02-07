4c42a53f7a84943bdd5cc11c9503fe63e86c1a11
/dev/null <106831060+AlexeyDmitrich@users.noreply.github.com>
2023-02-07 10:34:12 +0300

    Update homeTask22.py
    22task completed

--- seminar4/homeTask22.py
+++ seminar4/homeTask22.py
@@ -8,13 +8,53 @@ from random import randint as rnd
 
 #users_numbers_len = int(input("1st len: "))
 
-def create_set (len):    
-    users_numbers = set()
+def create_list_of_nums (len):    
+    users_numbers = []
     for i in range (len):
-        users_numbers.add(rnd(-100,100))
+        users_numbers.append(rnd(-100,100))
     return users_numbers
 
-kit1 = create_set(int(input("1st len: ")))
-print(kit1)
-kit2 = create_set(int(input("2nd len: ")))
-print(kit2)
+def find_mini (lst):
+    min = lst[0]
+    mini = 0
+    for i in range(len(lst)):
+        if (lst[i] < min):
+            min = lst[i]
+            mini = i
+    return mini
+
+def unite_lists (part_list1, part_list2):
+    part1, part2 = set(part_list1), set(part_list2)
+    united_set = part1.union(part2)
+    united_list = []
+    for element in united_set:
+        united_list.append(element)
+    return united_list
+
+def sort_to_kit (users_list1, users_list2):
+    users_list = unite_lists(users_list1, users_list2)
+    sorted_list = []
+    while (len(users_list) > 0):
+        min = find_mini(users_list)
+        sorted_list.append(users_list.pop(min))
+    return sorted_list
+
+def run ():
+    print("Для выхода введите 0")
+    try:    
+        kit1 = create_list_of_nums(int(input("Установите длину первого набора чисел: ")))
+        print(kit1)
+        kit2 = create_list_of_nums(int(input("Установите длину второго набора чисел: ")))
+        print(kit2)
+    except:
+        print("Введено неверное значение")
+        run()
+    if len(kit1) == 0 or len(kit2) == 0:
+        print("Завершение работы программы...")
+        exit()
+
+    sorted_kit = sort_to_kit (kit1, kit2)
+    print (sorted_kit)
+
+while True:
+    run()
