61d03dd49b779f596ffccc0633ab5387ac2cc1be
/dev/null <106831060+AlexeyDmitrich@users.noreply.github.com>
2023-02-08 11:17:10 +0300

    Update hard.py
    it can code string

--- seminar4/hard.py
+++ seminar4/hard.py
@@ -1,6 +1,5 @@
 # RLE aaaaabbbb = 5a4b
-
-users_string = input("Введите строку для кодирования/декодирования:\n")
+# RLE aaaaabbbb = 5a4b
 
 def string_to_list (input_string):
     blocks = [ [] ]
@@ -15,6 +14,33 @@ def string_to_list (input_string):
                 blocks.append([input_string[i]])
                 j+=1
             break
-    print(blocks)
+#    for block in blocks:
+#        print(block)
+    return blocks
 
-string_to_list(users_string)
+def code (list_of_blocks):
+    code_list = []
+    for block in list_of_blocks:
+        if len(block) == 1:
+            sector = block[0]
+        else:
+            sector = str(len(block))+block[0]
+        code_list.append(sector)
+#    print (code_list)
+    return code_list
+
+def list_to_string (formated_list):
+    res = ''
+    for code in formated_list:
+        res += f"{code}"
+#    print (res)
+    return res
+
+def full_code (stdin):
+    blocks = string_to_list(stdin)
+    code_list = code (blocks)
+    pure_code = list_to_string (code_list)
+    return pure_code
+
+users_string = input("Введите строку для кодирования/декодирования:\n")
+print (full_code(users_string))
