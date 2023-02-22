d1469b1e1e1aa4aba5ca252d27f6b1609b416204
/dev/null <106831060+AlexeyDmitrich@users.noreply.github.com>
2023-02-22 11:06:14 +0300

    Update menu.py
    из функции изъят вечный цикл, так как циклится она теперь в файле telegram.py,
    кейсы теперь не просто инициализируют функции, а возвращают их возвращаемые значения в telegram.py для вывода пользователю.

--- seminar8/bot/menu.py
+++ seminar8/bot/menu.py
@@ -1,28 +1,28 @@
 import languageModule as lm
 import functions as func
-import telegram as tg
+#import telegram as tg
 
 def working (choise=''):
-    while True:
-#        choise = input("\n Введите команду \n (ну или попросите помочь) \n")
-        choise = lm.translator(choise) # TODO: translator
-        match (choise):
-            case '/stop':
-                yes = 'сохранитьдавайугуагаможнонаверноехзмбyesyeahsave'
-                request = input("Сохранить сеанс перед выходом?\n")
-                if request.lower() in yes:
-                    func.save()
-                print('Не забывайте добавлять новые навыки! \n До новых встреч!')
-                break
-            case '/help':
-                func.print_help()
-            case '/addvac':
-                func.add_vacancy()
-            case '/addskill':
-                func.add_skill()
-            case '/allvac':
-                func.allpreview(func.base_of_vacancis)
-            case '/allskill':
-                func.allpreview(func.base_of_skills)
-            case '/rate':
-                func.rate()
+#    while True:
+    #        choise = input("\n Введите команду \n (ну или попросите помочь) \n")
+    choise = lm.translator(choise) # TODO: translator
+    match (choise):
+        case '/stop':
+            yes = 'сохранитьдавайугуагаможнонаверноехзмбyesyeahsave'
+            request = input("Сохранить сеанс перед выходом?\n")
+            if request.lower() in yes:
+                func.save()
+            print('Не забывайте добавлять новые навыки! \n До новых встреч!')
+#                break
+        case '/help':
+            return func.print_help()
+        case '/addvac':
+            return func.add_vacancy()
+        case '/addskill':
+            return func.add_skill()
+        case '/allvac':
+            return func.allpreview(func.base_of_vacancis)
+        case '/allskill':
+            return func.allpreview(func.base_of_skills)
+        case '/rate':
+            return func.rate()
